6331d8a3d4fa27e7793621d452ade0e3ea3c3179
DESKTOP-7HAA6M9\Administrator <albertlin94821@gmail.com>
2026-04-28 11:44:53 +0800

    Increase API body size limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 import { getCorsOrigins } from './cors-origins';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
 
   app.enableCors({
     origin: getCorsOrigins(),
